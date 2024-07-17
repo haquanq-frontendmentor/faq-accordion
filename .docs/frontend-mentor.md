9ca03f8153bc1d182d25191def035c1bf7b06958
Ha Quanq <quangha0120010@gmail.com>
2024-07-17 11:21:17 +0700

    Redo

---
+++ .docs/frontend-mentor.md
@@ -0,0 +1,26 @@
+## Frontendmentor.io solution post
+
+### What are you most proud of, and what would you do differently next time?
+
+Hello, this solution is an improved version of the old one, this time i made the source code a bit cleaner and more maintainable, also added accessibility to accordion 😁
+
+🏄 **Built with**
+
+- Semantic HTML
+- Pure CSS
+- Vanilla JavaScript
+- ARIA APG
+
+👀 **What i have done**
+
+- Used my own judgement to achieve **pixel-perfect** without design files
+- Built an accordion component with accessiblity for screen readers
+- Overridden default focus effect with outline on button
+
+### What challenges did you encounter, and how did you overcome them?
+
+None
+
+### What specific areas of your project would you like help with?
+
+Any feedback is appreciated 🙏
---
+++ app/main.js
@@ -0,0 +1,87 @@
+const accordion = {
+    btnElements: document.querySelectorAll(".faq__btn"),
+    contentElements: document.querySelectorAll(".faq__content"),
+    init() {
+        const transitionDuration = 300;
+
+        this.btnElements.forEach((faqBtn, index) => {
+            let requestingFrameId = null;
+
+            const faqContentElement = this.contentElements[index];
+            if (faqBtn.getAttribute("aria-expanded") == "true") {
+                faqContentElement.style.height = `auto`;
+            }
+
+            const hideContent = () => {
+                faqBtn.setAttribute("aria-expanded", "false");
+                const currentHeight = faqContentElement.clientHeight;
+                faqContentElement.style.height = `${currentHeight}px`;
+                faqContentElement.style.transition = `height ${transitionDuration}ms ease`;
+
+                if (requestingFrameId !== null) {
+                    cancelAnimationFrame(requestingFrameId);
+                    requestingFrameId = null;
+                }
+                requestAnimationFrame(() => {
+                    faqContentElement.style.height = "0";
+
+                    let startCleanUpTime;
+                    const cleanUpRequest = (t) => {
+                        if (!startCleanUpTime) startCleanUpTime = t;
+                        if (t - startCleanUpTime >= transitionDuration) {
+                            faqContentElement.style.height = "";
+                            faqContentElement.style.transition = "";
+                            faqContentElement.setAttribute("hidden", "");
+                            requestingFrameId = null;
+                            return;
+                        }
+                        requestingFrameId = requestAnimationFrame(cleanUpRequest);
+                    };
+                    requestingFrameId = requestAnimationFrame(cleanUpRequest);
+                });
+            };
+
+            const showContent = () => {
+                faqBtn.setAttribute("aria-expanded", "true");
+                faqContentElement.removeAttribute("hidden");
+                const currentHeight = faqContentElement.clientHeight;
+                faqContentElement.style.height = "auto";
+                const targetHeight = faqContentElement.clientHeight;
+
+                if (requestingFrameId != null) {
+                    cancelAnimationFrame(requestingFrameId);
+                    requestingFrameId = null;
+                    faqContentElement.style.height = `${currentHeight}px`;
+                } else {
+                    faqContentElement.style.height = "0";
+                }
+
+                requestAnimationFrame(() => {
+                    faqContentElement.style.transition = `height ${transitionDuration}ms ease`;
+                    faqContentElement.style.height = `${targetHeight}px`;
+
+                    let startCleanUpTime;
+                    const cleanUpRequest = (t) => {
+                        if (!startCleanUpTime) startCleanUpTime = t;
+                        if (t - startCleanUpTime >= transitionDuration) {
+                            faqContentElement.style.height = "";
+                            faqContentElement.style.transition = "";
+                            requestingFrameId = null;
+                            return;
+                        }
+                        requestingFrameId = requestAnimationFrame(cleanUpRequest);
+                    };
+                    requestingFrameId = requestAnimationFrame(cleanUpRequest);
+                });
+            };
+
+            faqBtn.addEventListener("click", (e) => {
+                const expanded = faqBtn.getAttribute("aria-expanded") === "true";
+                if (expanded) hideContent();
+                else showContent();
+            });
+        });
+    },
+};
+
+accordion.init();
